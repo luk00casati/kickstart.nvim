e1d77ce78f24a27cec47dc7b141b57cc640dd801
Luca Casati <luk00casati@gmail.com>
2024-10-07 14:26:47 +0200

    initlsp

--- init.lua
+++ init.lua
@@ -541,8 +541,46 @@ require('lazy').setup({
     icons = vim.g.have_nerd_font and {}
   },
 })
+-- more lsp
+local lsp_zero = require('lsp-zero')
 
-require 'kickstart.plugins.lsp'
+local lsp_attach = function(client, bufnr)
+  lsp_zero.default_keymaps({
+    buffer = bufnr,
+    preserve_mappings = false
+  })
+end
+
+lsp_zero.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = lsp_attach,
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
+})
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'lua_ls', 'rust_analyzer', 'clangd'},
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  }
+})
+
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  snippet = {
+    expand = function(args)
+      -- You need Neovim v0.10 to use vim.snippet
+      vim.snippet.expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({}),
+})
 
 vim.cmd.colorscheme(THEME)
 vim.cmd.hi 'Comment gui=none'
